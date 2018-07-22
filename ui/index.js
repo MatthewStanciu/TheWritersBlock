0518d72921a0affc0a24fc81d94dd469a176b740
TechBug2012 <mattbstanciu@gmail.com>
2018-07-21 20:56:17 -0500

    bit of formatting work

--- ui/index.js
+++ ui/index.js
@@ -1,10 +1,10 @@
 var Web3 = require('web3');
 var web3 = new Web3();
 
-if(typeof web3 != 'undefined'){
+if (typeof web3 != 'undefined') {
          console.log("Using web3 detected from external source like Metamask")
          this.web3 = new Web3(web3.currentProvider)
-      }else{
+      } else {
          console.log("No web3 detected. Falling back to http://localhost:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
          this.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"))
 }
